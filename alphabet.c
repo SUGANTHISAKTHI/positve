b5525026008e5c008cb918d46a58fa6d3ecdc7e5
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-03-20 14:40:10 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int c;
+scanf("%c",&c);
+if((c>='A'&& c<='Z')||(c>='a' &&c <='z'))
+printf("The character is alphabet");
+else
+printf("The character is not an alphabet");
+
+}
