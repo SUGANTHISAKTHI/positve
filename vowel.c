2d98e57c9a37c999f704aadd0e298fda02b83428
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-03-20 14:39:06 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    char c;
+    scanf("%c",&c);
+    if(c =='a' || c =='A' || c =='e' || c=='E' ||c=='i' || c=='i' || c=='o' ||c=='O' ||c=='u' ||c=='U')
+    printf("the character is vowel");
+    else
+    printf("The character is consonant");
+
+}
