66500bc13ffad117cd20fd028793f4652cec7318
SUGANTHISAKTHI <suganthi.scse@ksrce.ac.in>
2017-03-20 14:35:12 +0530

    Add files via upload

---
+++ poisitive.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    {
+
+        printf("The number is poisitive \n");
+    } else if(n<0)
+    {
+        printf("The number is negative \n");
+    }else
+    printf("The number is zero \n");
+}
